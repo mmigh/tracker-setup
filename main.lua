9f3ccb4208252476a413ef93762aaf68b4e15d9c
Primal_han <hieu30671@gmail.com>
2025-06-08 06:48:05 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/hermanos.lua"))()
+task.delay(10, function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/xero.lua"))()
+end)
