532aa82f88aa68e03ce40ebd1b9921ada7395b63
Primal_han <hieu30671@gmail.com>
2025-06-08 06:52:04 +0700

    Update xero.lua

--- xero.lua
+++ xero.lua
@@ -1,6 +1,6 @@
 getgenv().Team = "Pirates"
 getgenv().Xero_Setting = {
-    ['Key'] = getgenv().xero_key,
+    ['Key'] = getgenv().Key_2,
     ['Device'] = 'CHANGE-ME',
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xero2409/Panel/refs/heads/main/panel.lua"))()
