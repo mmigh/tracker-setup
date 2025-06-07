267e8b2b8ee2e544339759bc64a89f046a101cb8
Primal_han <hieu30671@gmail.com>
2025-06-08 06:47:35 +0700

    Delete main.lua

--- main.lua
+++
@@ -1,4 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/hermanos.lua"))()
-task.delay(10, function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/xero.lua"))()
-end)
