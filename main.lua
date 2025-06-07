07ea4da871cf72b780951e951040faa53d468be7
Primal_han <hieu30671@gmail.com>
2025-06-08 06:45:21 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,3 @@
-getgenv().hermanos_key = "YOUR_HERMANOS_KEY"
-getgenv().xero_key = "YOUR_XERO_KEY"
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/hermanos.lua"))()
 task.delay(10, function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/xero.lua"))()
