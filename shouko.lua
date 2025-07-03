0a9883677d015ab7a5afac08146f69579af25ab8
Primal_han <hieu30671@gmail.com>
2025-07-03 17:43:16 +0700

    Update shouko.lua

--- shouko.lua
+++ shouko.lua
@@ -1,6 +1,6 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Setting = getgenv().Setting or {
-    ["Discord ID"] = "",
+    ["Discord ID"] = getgenv().Key_3,
     ["Device Name"] = "ROKID_01"
 }
 loadstring(game:HttpGet("https://repo.rokidmanager.com/RokidManager/neyoshiiuem/main/trackstat.lua"))()
