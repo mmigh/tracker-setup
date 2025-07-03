dd6c04b45d06d90321fad8c37085baae374ed8cb
Primal_han <hieu30671@gmail.com>
2025-07-03 17:48:54 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/hermanos.lua"))()
-task.delay(10, function()
+
+task.delay(5, function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/xero.lua"))()
-task.delay(10, function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/shouko.lua"))()
+
+    task.delay(5, function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/mmigh/tracker-setup/refs/heads/main/shouko.lua"))()
+    end)
 end)
